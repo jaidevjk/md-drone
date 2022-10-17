1487f5f47964c7ffe9745463d338d051d9e5daaa
jaidevjk <jaidevjk316@gmail.com>
2022-10-17 13:49:26 +0530

    git_manifesto

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // for connecing frontend in heroku
 app.use(express.static(path.join(__dirname, 'client','build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/public', 'index.html'));
 });
 
