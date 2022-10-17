4bb60d2d98667e41e2ede3bc8b02a2dd11e7e176
jaidevjk <jaidevjk316@gmail.com>
2022-10-17 10:35:56 +0530

    commited

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.use('/', indexRouter);
