0393f209823729e5370f36873a01198dc1947e86
jaidevjk <jaidevjk316@gmail.com>
2022-10-17 14:53:57 +0530

    buildfile

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client','build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 //app.use('/', indexRouter);
